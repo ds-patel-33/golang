3752f6d0860a7b2362c4169b3c1577b10264c698
Dhiraj <pateldhiraj782@gmail.com>
2020-10-27 20:39:47 +0530

    sql_database updated code

--- Sql_Database/main.go
+++ Sql_Database/main.go
@@ -1,87 +1,62 @@
 package main
- 
+
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
- 
-type Post struct {
-    Id   int
-    Name string
-    Text string
-}
- 
+
 func main() {
-    db, e := sql.Open("mysql", "rootuser:dhirajpatel@/posts")
-    ErrorCheck(e)
- 
-    // close database after all work is done
-    defer db.Close()
- 
-    PingDB(db)
- 
-    // INSERT INTO DB
-    // prepare
-    stmt, e := db.Prepare("insert into posts(id, Name, Text) values (?, ?, ?)")
-    ErrorCheck(e)
- 
-    //execute
-    res, e := stmt.Exec("5", "Post five", "Contents of post 5")
-    ErrorCheck(e)
- 
-    id, e := res.LastInsertId()
-    ErrorCheck(e)
- 
-    fmt.Println("Insert id", id)
- 
-    //Update db
-    stmt, e := db.Prepare("update posts set Text=? where id=?")
-    ErrorCheck(e)
- 
-    // execute
-    res, e := stmt.Exec("This is post five", "5")
-    ErrorCheck(e)
- 
-    a, e := res.RowsAffected()
-    ErrorCheck(e)
- 
-    fmt.Println(a)
- 
-    // query all data
-    rows, e := db.Query("select * from posts")
-    ErrorCheck(e)
- 
-    var post = Post{}
- 
-    for rows.Next() {
-        e = rows.Scan(&post.Id, &amp;post.Name, &post.Text)
-        ErrorCheck(e)
-        fmt.Println(post)
-    }
- 
-    // delete data
-    stmt, e := db.Prepare("delete from posts where id=?")
-    ErrorCheck(e)
- 
-    // delete 5th post
-    res, e := stmt.Exec("5")
-    ErrorCheck(e)
- 
-    // affected rows
-    a, e := res.RowsAffected()
-    ErrorCheck(e)
- 
-    fmt.Println(a) // 1
-}
- 
-func ErrorCheck(err error) {
-    if err != nil {
-        panic(err.Error())
-    }
-}
- 
-func PingDB(db *sql.DB) {
-    err := db.Ping()
-    ErrorCheck(err)
+
+	db, err := sql.Open("mysql", "root:dhirajpatel@tcp(127.0.0.1:3306)/employees")
+	fmt.Println(db)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	tx, _ := db.Begin()
+	stmt, err := tx.Prepare("INSERT INTO employees(id,username) VALUES(?,?)")
+	res, err := stmt.Exec(4, "Dhiraj")
+	res, err = stmt.Exec(56, "Rohit")
+	if err != nil {
+		tx.Rollback()
+		log.Fatal(err)
+	}
+	tx.Commit()
+	log.Println(res)
+
+	rows, err := db.Query("select id, username from employees where id = ?", 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		id   int
+		name string
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+
+	sqlStatementUpdt := `
+    UPDATE employees
+    SET username = "Shubham" WHERE id=56;`
+	result, err := db.Exec(sqlStatementUpdt)
+	if err != nil {
+		panic(err)
+	}
+	count, err := result.RowsAffected()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("rows updated: %v\n", count)
+
 }
